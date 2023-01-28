23f5d0c81131814f7412e21d510b511efbb9c833
Borko Rajkovic <rajkovicborko@gmail.com>
2023-01-28 02:50:06 +0100

    Prime

---
+++ prime.js
@@ -0,0 +1,13 @@
+function isPrime(number) {
+    if (number == 2) {
+        return true;
+    }
+
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
