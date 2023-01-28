283ccbc19a49d9771c30ed965c33127dc9bb0f6d
Borko Rajkovic <rajkovicborko@gmail.com>
2023-01-28 16:40:51 +0100

    Reduce for loop to a square root of number

--- prime.js
+++ prime.js
@@ -7,7 +7,7 @@ function isPrime(input) {
         return true;
     }
 
-    for (let i = 2; i < number; i++) {
+    for (let i = 2; i < Math.sqrt(number); i++) {
         if (number % i === 0) {
             return false;
         }
